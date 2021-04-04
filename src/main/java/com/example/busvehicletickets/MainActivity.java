9d923e149a1f68e29298058370672a2e4a7f53ac
Ramazan <ramazan.halid.35@gmail.com>
2021-04-05 01:26:02 +0300

    First commit

---
+++ src/main/java/com/example/busvehicletickets/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.busvehicletickets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView textViewser = (TextView) findViewById(R.id.main_textViewUser);
+        textViewser.setText("Welcome, " + mAuth.getCurrentUser().getEmail() );
+    }
+
+    public void logout(View view){
+        mAuth.signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
+

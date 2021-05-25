ab2342e6d44f977227572b190b3b105c21db70a4
Ramazan <ramazan.halid.35@gmail.com>
2021-05-25 22:43:03 +0300

    Bought ticket list added

--- src/main/java/com/example/busvehicletickets/BoughtTicketActivity.java
+++ src/main/java/com/example/busvehicletickets/BoughtTicketActivity.java
@@ -29,8 +29,50 @@ public class BoughtTicketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bought_ticket);
 
+        listView = (ListView) findViewById(R.id.search_resultList);
+        travelDtoArrayList = new ArrayList<>();
+       // travelDtoArrayList.add(new TravelDto("asdfaf","asdfaf","asdfaf","asdfaf","asdfaf","asdfaf","asdfaf"));
+
+        myRef.collection("users")
+
+                .document("2uXLbB9kMYdr50RSddgIOKnx3uu2")
+                //.document(mAuth.getCurrentUser().getUid())
+
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        UserDto userDto = documentSnapshot.toObject(UserDto.class);
+                        ArrayList<TicketDto> ticketDtoArrayList = userDto.getTicketDtoArrayList();
+
+                        for (int i = 0; i < ticketDtoArrayList.size(); i++) {
+                            if (ticketDtoArrayList.get(i).getStatusOfTicket().equals("bought")){
+                                travelDtoArrayList.add(ticketDtoArrayList.get(i).getTravelDto());
+
+                            }
+
+                           // travelDtoArrayList.add(new TravelDto("asdfaf","asdfaf","asdfaf","asdfaf","asdfaf","asdfaf","asdfaf"));
+                            mAdapter = new TravelDtoAdapter(BoughtTicketActivity.this,travelDtoArrayList);
+                            listView.setAdapter(mAdapter);
+                            listView.setOnItemClickListener(listClick);
+                        }
+
+
+
+
+
+                    }
+
+
+                });
 
 
     }
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+
+        }
+    };
 }
